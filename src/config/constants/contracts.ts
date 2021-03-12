37a74c7f767e200ff7236429ca48a5d12b79d157
Akshay <akshayk@eiysys.in>
2021-03-12 13:52:12 +0530

    pharms

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+    97: '0xea6cb572b6b194b794da9b6881ad175e2325db84',
+  },
+  masterChef: {
+    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    97: '0x0ec5eee040b9b3600dc1c0decaae50a89942e91b',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '0x58AbBb3c89750dDA42b65822042EbADb00f9Ef61',
+  },
+  lottery: {
+    56: '',
+    97: '0x1a198790bcf16c20d913ab3977ecd0c798b4591c',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '0x979a0bb189afbd8fb20f4385a7d2bac5e18f442f',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0xC19831b355003A60093E3A37dbCA80C1e8605309',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '0xa2D2b501E6788158Da07Fa7e14DEe9F2C5a01054',
+  },
+}
